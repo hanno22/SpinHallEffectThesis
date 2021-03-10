403c3e0e6c217eb45f96fc88e5c4a5b0f8efb632
hchr <h.chr@icloud.com>
2021-03-10 18:51:03 +0100

    -hough circle algo

--- main.py
+++ main.py
@@ -2,26 +2,44 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from scipy.ndimage.interpolation import geometric_transform
 import numpy as np
+import cv2 as cv
 
-probe_number = '1'
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
-img = [
-    rgb2gray(mpimg.imread('data/' + probe_number + '/bfp_bb.bmp')),
-    #mpimg.imread('data/bfp.bmp'),
-    #rgb2gray(mpimg.imread('/data2/fp_bb_dark.bmp'))
-]
+img = cv.imread('data/2/bfp_bb.bmp', 0)
+#img = cv.medianBlur(img, 15)
+cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
 
-def topolar(data, order=1):
+circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 10, param1=30, param2=20, minRadius=410, maxRadius=430)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv.circle(img,(i[0],i[1]),2,(0,0,255),3)
+
+# cv.imshow('detected circles',img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+# probe_number = '1'
+# def rgb2gray(rgb):
+#     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+#
+# img = [
+#     rgb2gray(mpimg.imread('data/' + probe_number + '/bfp_bb.bmp')),
+#     #mpimg.imread('data/bfp.bmp'),
+#     #rgb2gray(mpimg.imread('/data2/fp_bb_dark.bmp'))
+# ]
+
+def topolar(data, center, order=1):
     max_radius = 0.5*np.linalg.norm(data.shape)
 
     def transform(coords):
-        theta = 2*np.pi*coords[1] / (data.shape[1]) #'- 1.)
+        theta = 2*np.pi*coords[1] / (data.shape[1]) #- 1.)
         radius = max_radius * coords[0] / data.shape[0]
 
-        i = 0.5*data.shape[0] - radius*np.sin(theta)
-        j = radius*np.cos(theta) + 0.5*data.shape[1]
+        i = data.shape[0] - radius*np.sin(theta) - center[0]
+        j = radius*np.cos(theta) + data.shape[1] - center[1]
         return i, j
 
     polar = geometric_transform(data, transform, order=order)
@@ -30,7 +48,9 @@ def topolar(data, order=1):
 
     return polar, (rads, angs)
 
-pol, (rads, angs) = topolar(img[0], order=5)
+pol, (rads, angs) = topolar(img, center= (100,100), order=5)
+plt.imshow(img)
+plt.show()
 null_indices = np.where(~pol.any(axis=1))[0]
 pol = np.delete(pol, null_indices, 0)
 rads = np.delete(rads, null_indices)
