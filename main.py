e86c30a431f8d34104493250d560ffd227003f5a
hchr <h.chr@icloud.com>
2021-03-10 17:12:05 +0100

    -added simple evaluation Routine -transform to polar coordinates -radialProfile

--- main.py
+++ main.py
@@ -2,89 +2,60 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from scipy.ndimage.interpolation import geometric_transform
 import numpy as np
-from matplotlib.pyplot import figure
-#figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+
+probe_number = '1'
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
-#https://stackoverflow.com/questions/3798333/image-information-along-a-polar-coordinate-system/3806851
+
 img = [
-    rgb2gray(mpimg.imread('data/bfp_bb.bmp')),
+    rgb2gray(mpimg.imread('data/' + probe_number + '/bfp_bb.bmp')),
     #mpimg.imread('data/bfp.bmp'),
-    rgb2gray(mpimg.imread('data/fp_bb_dark.bmp'))
+    #rgb2gray(mpimg.imread('/data2/fp_bb_dark.bmp'))
 ]
 
-for im in img:
-    plt.imshow(im)# ,cmap="binary")
-    plt.colorbar()
-    plt.show()
-
-
-def cart2polar(x, y):
-    r = np.sqrt(x**2 + y**2)
-    theta = np.arctan2(y, x)
-    return r, theta
-
-def polar2cart(r, theta):
-    x = r * np.cos(theta)
-    y = r * np.sin(theta)
-    return x, y
-
-
-def radial_profile(data, center):
-    y, x = np.indices((data.shape))
-    r = np.sqrt((x - center[0])**2 + (y - center[1])**2)
-    r = r.astype(np.int)
-
-    tbin = np.bincount(r.ravel(), data.ravel())
-    nr = np.bincount(r.ravel())
-    radialprofile = tbin / nr
-    return radialprofile
-
-def topolar(img, order=1):
-    """
-    Transform img to its polar coordinate representation.
-
-    order: int, default 1
-        Specify the spline interpolation order.
-        High orders may be slow for large images.
-    """
-    # max_radius is the length of the diagonal
-    # from a corner to the mid-point of img.
-    max_radius = 0.5*np.linalg.norm( img.shape )
+def topolar(data, order=1):
+    max_radius = 0.5*np.linalg.norm(data.shape)
 
     def transform(coords):
-        # Put coord[1] in the interval, [-pi, pi]
-        theta = 2*np.pi*coords[1] / (img.shape[1] - 1.)
+        theta = 2*np.pi*coords[1] / (data.shape[1]) #'- 1.)
+        radius = max_radius * coords[0] / data.shape[0]
 
-        # Then map it to the interval [0, max_radius].
-        #radius = float(img.shape[0]-coords[0]) / img.shape[0] * max_radius
-        radius = max_radius * coords[0] / img.shape[0]
+        i = 0.5*data.shape[0] - radius*np.sin(theta)
+        j = radius*np.cos(theta) + 0.5*data.shape[1]
+        return i, j
 
-        i = 0.5*img.shape[0] - radius*np.sin(theta)
-        j = radius*np.cos(theta) + 0.5*img.shape[1]
-        return i,j
-
-    polar = geometric_transform(img, transform, order=order)
-
-    rads = max_radius * np.linspace(0,1,img.shape[0])
-    angs = np.linspace(0, 2*np.pi, img.shape[1])
+    polar = geometric_transform(data, transform, order=order)
+    rads = max_radius * np.linspace(0, 1, data.shape[0])
+    angs = np.linspace(0, 2*np.pi, data.shape[1])
 
     return polar, (rads, angs)
 
-#profile_data = radial_profile(img[0], [len(img[0][0])/2, len(img[0][1])/2])
-
-
-#plt.plot(profile_data)
-#plt.show()
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='polar')
 pol, (rads, angs) = topolar(img[0], order=5)
-cs = ax.contourf(angs, rads, pol, cmap='plasma')
-fig.colorbar(cs)
+null_indices = np.where(~pol.any(axis=1))[0]
+pol = np.delete(pol, null_indices, 0)
+rads = np.delete(rads, null_indices)
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111, projection='polar')
+NA = 1.216
+radial_profile = np.average(pol, axis=1, weights= pol >0)
+index_max = np.argmax(radial_profile)
+index_NA = 710
+radial_axis = np.arange(0, NA/index_NA * len(rads), NA/index_NA)
+
+cs = ax1.contourf(angs, radial_axis, pol, cmap='binary')
+fig1.colorbar(cs)
+#fig1.safefig('results/' + probe_number + '/polarplot.png')
 plt.show()
 
-radial_profile = np.average(pol, axis=1, weights= pol >0)
-plt.plot(radial_profile)
+
+
+
+extra_ticks = [radial_axis[index_max], radial_axis[index_NA]]
+fig2, ax2 = plt.subplots()
+ax2.plot(radial_axis, radial_profile, '.')
+ax2.axvline(radial_axis[index_max], linestyle='--', linewidth=0.5)
+ax2.axvline(radial_axis[index_NA], linestyle='--', linewidth=0.5)
+#ax2.set_xticks(list(ax2.get_xticks()) + extra_ticks)
+print(radial_axis[index_max])
 plt.show()
